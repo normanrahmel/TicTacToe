90eba4ae60d86796ccba7ce4f5de7386a0915020
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-05-02 20:30:22 +0200

    basic framework

---
+++ script.js
@@ -0,0 +1,25 @@
+let fields = [];
+let currentShape = 'cross';
+
+function fillShape(id) {
+    if (currentShape == 'corss') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
+    }
+    fields[id] = currentShape;
+    console.log(fields)
+    draw();
+}
+
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none')
+        }
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none')
+        }
+    }
+}
